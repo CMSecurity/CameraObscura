34c75f35b80c945a01bd847d7729df89edb39dc9
cmllr <fury224@googlemail.com>
2018-02-17 12:07:34 +0100

    Fixed logging

--- core/actions/log.py
+++ core/actions/log.py
@@ -7,6 +7,7 @@ Creates a log entry
 
 from flask import Flask, request
 from core import logging
+import datetime
 
 def run(app: Flask, route: object, request: request, sessionId: str):
-    logging.log(logging.EVENT_ID_HTTP, datetime.now(), "HTTP-{0} Request".format(request.method), "http", False, request.remote_addr,0.0, sessionId)
+    logging.log(logging.EVENT_ID_HTTP, datetime.datetime.now(), "{0} Request".format(request.method), "http", False, request.remote_addr,0.0, sessionId)
