411f173ecfd5dfe605f95c3527a00e4af9f4d301
cmllr <fury224@googlemail.com>
2020-02-12 19:40:22 +0100

    Removed init remarks as it does not exist anymore

--- main.py
+++ main.py
@@ -8,14 +8,9 @@ Program main entrance point
 import sys
 from os.path import isfile, join
 import termcolor
-from core import http, config, init
-
-if len(sys.argv) == 2 and sys.argv[1] == "init":
-    init.init()
-    sys.exit(0)
+from core import http, config
 
 if isfile(join(config.ROOT, "configuration.cfg")) is False:
-    print(termcolor.colored("We recommend to initalize the honeypot", "yellow"))
-    print("You can do this with python3 main.py init")
+    raise Exception("configuration.cfg is missing")
 else:
     http.serve(__file__)
