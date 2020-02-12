ef9c2cd8b271e994b5ee9db4e4efe2f1cdcdf079
cmllr <fury224@googlemail.com>
2020-02-12 19:38:03 +0100

    Removed authorize action as it's to specific to be included in a generic approach (will need generic run-xy action) Removed log action as logging is included in almost every action

--- core/actions/__init__.py
+++ core/actions/__init__.py
@@ -12,9 +12,7 @@ __all__ = [
     'catchfile',
     'servefile',
     'text',
-    'authorize',
-    'sleep',
-    'log'
+    'sleep'
 ]
 
 
@@ -22,19 +20,16 @@ def isAllowed(action: str):
     return action in __all__
 
 
-def run(action, app, path, route, request, sessionId):
+def run(action, app, path, route, request):
     if isAllowed(action) == False:
         logging.log(
             logging.EVENT_ID_ACTION_NOT_FOUND,
             datetime.now(),
             "Action not found {0}".format(action),
-            "http",
             True,
-            request.remote_addr,
-            0.0,
-            sessionId)
+            request.remote_addr,)
         return None
     modules = globals()
     if action in modules:
-        return modules[action].run(app, path, route, request, sessionId)
+        return modules[action].run(app, path, route, request)
     return None
