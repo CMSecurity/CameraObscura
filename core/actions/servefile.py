326faf2dadf854958d3c3fe3efea247ebeba93dd
cmllr <fury224@googlemail.com>
2022-05-07 13:41:36 +0200

    Add watermark to servefile action

--- core/actions/servefile.py
+++ core/actions/servefile.py
@@ -11,12 +11,21 @@ from datetime import datetime
 from os.path import join
 import pathlib
 from flask import Flask, request, send_file, render_template
-from core import config, logging, http
-
+from core import config, http
+from PIL import ImageDraw, Image
 
 def run(app: Flask, selectedPath: str, route: object,
         request: request):
     file = route["servefile"]["file"]
+    variables = {
+        "day": datetime.now().day,
+        "hour": datetime.now().hour,
+        "minute": datetime.now().minute,
+        "month": datetime.now().month,
+        "second": datetime.now().second,
+        "year": datetime.now().year,
+        "date": str(datetime.now())
+    }
     if "(" in selectedPath:
         match = re.search(selectedPath, request.path)
         if match is not None:
@@ -51,17 +60,32 @@ def run(app: Flask, selectedPath: str, route: object,
                         value = config.getConfigurationValue(
                             parts[0], parts[1])
                     else:
-                        variables = {
-                            "day": datetime.now().day,
-                            "hour": datetime.now().hour,
-                            "minute": datetime.now().minute,
-                            "month": datetime.now().month,
-                            "second": datetime.now().second,
-                            "year": datetime.now().year
-                        }
                         if name in variables:
                             value = str(variables[name])
                     if value is not None:
                         content = content.replace(match, value)
         return content
+    watermark = "watermark" in route["servefile"]
+    if watermark:
+        watermark_obj = route["servefile"]["watermark"]
+        x_coord = watermark_obj["x"]
+        y_coord = watermark_obj["y"]
+        text = watermark_obj["text"]
+        im = Image.open(fileToServe)
+        
+        if text in variables:
+            text = str(variables[text])
+        ImageDraw.Draw(
+         im 
+        ).text(
+            (x_coord, y_coord),  # Coordinates
+            text,
+            (0, 0, 0)  # Color
+        )
+        mime = im.get_format_mimetype()
+        format = im.format
+        response = None
+        im.save(fileToServe + "obscura", format)
+        response = send_file(fileToServe + "test", mimetype=mime, as_attachment=False,attachment_filename="image")
+        return response
     return send_file(fileToServe, as_attachment=False)
